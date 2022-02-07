e1d9ed45a511df44d1b059e5397c7df9a42eb025
Nicholas Schoeffler <nwschoeffler@gmail.com>
2022-02-07 08:51:03 -0600

    finished

--- data_visualization_project.py
+++ data_visualization_project.py
@@ -5,10 +5,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 station_num=1
-with open('BigData2016.csv',newline ='') as csvfile:
+#opens the csvfile
+with open('2016VizData.csv',newline ='') as csvfile:
     plots = csv.reader(csvfile, delimiter = ',') 
     reader = csv.DictReader(csvfile)
-
+    #makes a list of months
     months = [1,2,3,4,5,6,7,8,9,10,11,12]
     day_x = []
     for i in range(366):
@@ -19,23 +20,43 @@ with open('BigData2016.csv',newline ='') as csvfile:
         def __init__(self,STID):
             self.STID= STID
             self.Maxlist = []
+            self.minlist = []
+            self.havglist = []
+            self.wsmxlist = []
 
         def fill_list (self,row):
             if row['STID'] == self.STID:
+                #TMAX list appending
                 if float(row['TMAX']) != -996.00:
                     self.Maxlist.append(float(row['TMAX']))
                 else:
                     self.Maxlist.append(self.Maxlist[-1])
+
+                #TMIN list appending
+                if float(row['TMIN']) != -996.00:
+                    self.minlist.append(float(row['TMIN']))
+                else:
+                    self.minlist.append(self.minlist[-1])
+
+                #HAVG list appending
+                if float(row['HAVG']) != -996.00:
+                    self.havglist.append(float(row['HAVG']))
+                else:
+                    self.havglist.append(self.havglist[-1])
+                    
+                #WSMX list appending
+                if float(row['WSMX']) != -996.00:
+                    self.wsmxlist.append(float(row['WSMX']))
+                else:
+                    self.wsmxlist.append(self.wsmxlist[-1])
 
     #creates stations and adds the names to a list
     ALTU = Station ("ALTU")
     BEAV = Station ("BEAV")
     TISH = Station ("TISH")
     NRMN = Station ("NRMN")
-    STIL = Station ("STIL")
-    
-    TULN = Station("TULN")
-    stations = [ARD2,BEAV,BOIS,CENT,NRMN,STIL,TISH,TULN,WOOD]
+    TULN = Station ("TULN")
+    stations = [BEAV,ALTU,NRMN,TISH,TULN]
     
     
     #fills station list
@@ -44,8 +65,6 @@ with open('BigData2016.csv',newline ='') as csvfile:
             station.fill_list(row)
             
     #user input
-    user_name = input("what would you like me to call you?")
-    print (f"Oh hello {user_name}!")
     certain_station_Y_N = input("would you like to see just one station? If so type 'Y' if not type 'N'")
     if certain_station_Y_N == "Y":
         for station in stations:
@@ -57,14 +76,22 @@ with open('BigData2016.csv',newline ='') as csvfile:
     if certain_station_Y_N == "N":
         for station in stations:
             plt.plot(day_x, station.Maxlist, label = station.STID)
-    Tmax_or_tmin_print = input("If you would like me to print only the Tmax type (1)\n if you would you like me to only print the Tmin type (2)\n if you would like me to print both type (3)")
-
-
-
-
+    Tmax_or_tmin_print = input("If you would like me to print only the Tmax type (1)\nif you would you like to see Tmin type (2)\nif you want to see the humity type (3)\nif you want to see only the rain totals type (4)")
+    for station in stations:
+        if Tmax_or_tmin_print == '1':
+            plt.ylabel('Max Temperature(°F)')
+            plt.plot(day_x, station.Maxlist)
+        if Tmax_or_tmin_print == '2':
+            plt.ylabel('Min Temperature(°F)')
+            plt.plot(day_x, station.minlist)
+        if Tmax_or_tmin_print == '3':
+            plt.ylabel('Humidity')
+            plt.plot(day_x, station.havglist)
+        if Tmax_or_tmin_print == '4':
+            plt.ylabel('rainfall')
+            plt.plot(day_x, station.wsmxlist)
 
 plt.xlabel('Days')
-plt.ylabel('Temperature(°F)')
 plt.title('Weather Report', fontsize = 20)
 plt.grid()
 plt.legend()
